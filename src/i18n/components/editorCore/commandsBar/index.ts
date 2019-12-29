4ce8b507273c1a86258aaac3bf87610ffe4ea6f8
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2019-12-30 00:06:34 +0800

    i18n static type check

--- src/i18n/components/editorCore/commandsBar/index.ts
+++ src/i18n/components/editorCore/commandsBar/index.ts
@@ -2,6 +2,7 @@ import { en } from 'src/i18n/components/editorCore/commandsBar/en';
 import { zhCN } from 'src/i18n/components/editorCore/commandsBar/zh-CN';
 import { pack } from 'src/i18n/pack';
 
-const commandsBarI18n = pack(zhCN, en);
+const check = () => zhCN;
 
+const commandsBarI18n = pack<ReturnType<typeof check>>(zhCN, en);
 export { commandsBarI18n };
