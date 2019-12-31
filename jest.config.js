c04ff423db69455b179b2ec0ec6195ecdd3a18f2
zhengrenzhe <zhengrenzhe.niujie@gmail.com>
2020-01-01 02:12:20 +0800

    add test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: 'ts-jest/presets/js-with-ts',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
+  coverageDirectory: 'test-coverage',
   globals: {
     'ts-jest': {
       isolatedModules: true,
